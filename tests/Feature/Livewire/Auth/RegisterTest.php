cff58eef2d277fdcac495f0dc76adf071fcf393e
Cawe541 <cawe.wewe04@gmail.com>
2024-04-04 14:43:50 -0300

    crm-1: should be able to register a new user in the system

---
+++ tests/Feature/Livewire/Auth/RegisterTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Livewire\Auth\Register;
+use Livewire\Livewire;
+
+use function Pest\Laravel\{assertDatabaseCount, assertDatabaseHas};
+
+it('should render the component', function () {
+    Livewire::test(Register::class)
+        ->assertOk();
+});
+
+it('should be able to register a new user in the system', function () {
+    Livewire::test(Register::class)
+        ->set('name', 'Cawe')
+        ->set('email', 'c@w.com')
+        ->set('email_confirmation', 'c@w.com')
+        ->set('password', 'password')
+        ->call('submit')
+        ->assertHasNoErrors();
+
+    assertDatabaseHas('users', [
+        'name'  => 'Cawe',
+        'email' => 'c@w.com',
+    ]);
+
+    assertDatabaseCount('users', 1);
+});
