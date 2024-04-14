5df064a374c72c64bb7419b0572d588283af5906
Cawe541 <cawe.wewe04@gmail.com>
2024-04-14 09:33:16 -0300

    crm-2: creating login page

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,14 @@
 <?php
 
-use App\Livewire\Auth\Register;
+use App\Livewire\Auth\{Login, Register};
 use App\Livewire\Welcome;
 use Illuminate\Support\Facades\{Auth, Route};
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', Welcome::class);
 Route::get('/register', Register::class)->name('auth.register');
-Route::get('/home', Welcome::class)->name('home');
 Route::get('/logout', fn () => Auth::logout())->name('logout');
+Route::get('/login', Login::class)->name('login');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/', Welcome::class);
+    Route::get('/home', Welcome::class)->name('home');
+});
