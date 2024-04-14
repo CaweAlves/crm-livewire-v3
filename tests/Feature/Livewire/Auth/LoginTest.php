99542c7b6bf3342d460ade15a9d8755eff34cfea
Cawe541 <cawe.wewe04@gmail.com>
2024-04-14 08:45:20 -0300

    CRM-2: ensuring login to be secured by rate limiting

--- tests/Feature/Livewire/Auth/LoginTest.php
+++ tests/Feature/Livewire/Auth/LoginTest.php
@@ -37,3 +37,21 @@ it('should make sure to inform the user an error when email and password doesnt 
         ->assertNoRedirect('home');
 
 });
+
+it('should make sure that the rate limiting is blocking after 5 attempts', function () {
+    $user = User::factory()->create();
+
+    for ($i = 0; $i < 5; $i++) {
+        Livewire::test(Login::class)
+            ->set('email', $user->email)
+            ->set('password', 'wrong-password')
+            ->call('tryToLogin')
+            ->assertHasErrors('invalidCredentials');
+    }
+
+    Livewire::test(Login::class)
+        ->set('email', $user->email)
+        ->set('password', 'wrong-password')
+        ->call('tryToLogin')
+        ->assertHasErrors('rateLimiter');
+});
